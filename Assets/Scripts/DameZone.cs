7cf111ea26cee02efff9fa050fd100152b179cea
a59647 <a59647@alunos.ipb.pt>
2025-03-20 16:30:27 +0100

    fix Health system

--- Assets/Scripts/DameZone.cs
+++ Assets/Scripts/DameZone.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DameZone : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class DamageZone : MonoBehaviour
+{
+   void OnTriggerStay2D(Collider2D other)
+   {
+       PlayerController controller = other.GetComponent<PlayerController>();
+
+
+       if (controller != null)
+       {
+           controller.ChangeHealth(-1);
+       }
+   }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,14 +15,8 @@ public class PlayerController : MonoBehaviour
 
   // Variables related to the health system
   public int maxHealth = 5;
-  int currentHealth;
   public int health { get { return currentHealth; }}
-
-
-  // Variables related to temporary invincibility
-  public float timeInvincible = 2.0f;
-  bool isInvincible;
-  float damageCooldown;
+  int currentHealth = 1;
 
 
   // Start is called before the first frame update
@@ -30,29 +24,17 @@ public class PlayerController : MonoBehaviour
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
-
-
-     currentHealth = maxHealth;
+     //currentHealth = maxHealth;
   }
  
   // Update is called once per frame
   void Update()
   {
      move = MoveAction.ReadValue<Vector2>();
+  }
 
 
-     if (isInvincible)
-     {
-         damageCooldown -= Time.deltaTime;
-         if (damageCooldown < 0)
-         {
-            isInvincible = false;
-         }
-     }
-   }
-
-
-// FixedUpdate has the same call rate as the physics system
+  // FixedUpdate has the same call rate as the physics system
   void FixedUpdate()
   {
      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -62,20 +44,8 @@ public class PlayerController : MonoBehaviour
 
   public void ChangeHealth (int amount)
   {
-     if (amount < 0)
-     {
-        if (isInvincible)
-        {
-           return;
-        }
-     isInvincible = true;
-     damageCooldown = timeInvincible;
-  }
-
-
      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
      Debug.Log(currentHealth + "/" + maxHealth);
   }
-
 
 }

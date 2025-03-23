ec40e85e48cf459d9298f49be30d77e9c18f47cb
a59647 <a59647@alunos.ipb.pt>
2025-03-23 12:27:11 +0100

    Play animations based on game data

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,76 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class EnemyController : MonoBehaviour
 {
-   // Public variables
-   public float speed;
-   public bool vertical;
-   public float changeTime = 3.0f;
-  
-   // Private variables
-   Rigidbody2D rigidbody2d;
-   float timer;
-   int direction = 1;
+    // Public variables
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
 
+    // Private variables
+    private Rigidbody2D rigidbody2d;
+    private Animator animator;
+    private float timer;
+    private int direction = 1;
 
-   // Start is called before the first frame update
-   void Start()
-   {
-       rigidbody2d = GetComponent<Rigidbody2D>();
-       timer = changeTime;
-   }
-
-
-   // Update is called every frame
-   void Update()
-   {
-     
-
-
-       timer-= Time.deltaTime;
-
-
-      if (timer < 0)
-      {
-        direction = -direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         timer = changeTime;
-      }
-   }
+    }
 
+    void Update()
+    {
+        timer -= Time.deltaTime;
 
+        // Change direction when the timer reaches zero
+        if (timer < 0)
+        {
+            direction = -direction; // Invert direction
+            timer = changeTime; // Reset timer
+        }
+    }
 
+    // FixedUpdate is called at a fixed interval (aligned with physics updates)
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
 
-  // FixedUpdate has the same call rate as the physics system
-  void FixedUpdate()
-  {    
-       Vector2 position = rigidbody2d.position;
-     
-       if (vertical)
-       {
-           position.y = position.y + speed * direction * Time.deltaTime;
-       }
-       else
-       {
-           position.x = position.x + speed * direction * Time.deltaTime;
-       }
+        // Move the enemy based on direction and orientation (vertical/horizontal)
+        if (vertical)
+        {
+            position.y += speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x += speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
 
+            // Flip the sprite horizontally based on direction
+            if (direction != 0)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(direction), 1, 1);
+            }
+        }
 
-       rigidbody2d.MovePosition(position);
-  }
+        rigidbody2d.MovePosition(position);
+    }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
 
-   void OnTriggerEnter2D(Collider2D other)
-   {
-       PlayerController player = other.gameObject.GetComponent<PlayerController>();
-
-
-       if (player != null)
-       {
-           player.ChangeHealth(-1);
-       }
-   }
-
-
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
 }
